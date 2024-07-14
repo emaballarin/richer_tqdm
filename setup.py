d93b4f4aa39073e9504cadc11f2e087f7d5deab3
Emanuele Ballarin <emanuele@ballarin.cc>
2024-07-14 02:47:19 +0200

    Minor README.md fixups
    Signed-off-by: Emanuele Ballarin <emanuele@ballarin.cc>

--- setup.py
+++ setup.py
@@ -18,9 +18,10 @@ def _fread(fname: str):
 
 setup(
     name="richer_tqdm",
-    version="0.0.1",
+    version="0.0.2",
     description="Improved tqdm.rich decorator for iterators and associated machinery.",
     long_description=_fread("README.md"),
+    long_description_content_type="text/markdown",
     keywords=["tqdm", "rich", "cli", "progress", "time", "bar", "progressbar"],
     author="Emanuele Ballarin",
     author_email="emanuele@ballarin.cc",
